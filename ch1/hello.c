6c61943dbc96ef3ae55e0a6e74dfc918d5db4fd1
초록꼬마 <greenkey20@gmail.com>
2022-06-26 17:22:15 +0900

    feat: add basic study codes about pointer

---
+++ ch1/hello.c
@@ -0,0 +1,9 @@
+/* 예제1-1: hello 출력
+* 파일 이름: hello.c
+* 수정 일자: 2022.6.24(금) 17h50
+* 작성자: greenpianorabbit
+*/
+#include <stdio.h> // header 파일 추가 
+void main() { // main() 함수 추가
+	printf("hello, C 프로그래밍! \n"); // 모니터에 문자열 출력 + 개행
+} // main() 함수 종료
---
+++ ch6/6-11/6-11.c
@@ -0,0 +1,10 @@
+/* 2022.6.26(일) 17h5 C 프로그래밍
+* p198 포인터 변수가 매개변수로 사용된 함수
+*/
+#include <stdio.h>
+int change(int i); // change() 함수의 원형 선언
+
+void main() {
+	int a = 10;
+	int *p = &a; // 포인터 변수 p 선언 + 변수 a의 주소를 저장
+}
