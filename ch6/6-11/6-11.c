f2b3a62e9c70e7dec2a1d13d2f9b4b2bb5433cdf
초록꼬마 <greenkey20@gmail.com>
2022-06-29 23:59:08 +0900

    feat: add an example of pointer

--- ch6/6-11/6-11.c
+++ ch6/6-11/6-11.c
@@ -7,4 +7,12 @@ int change(int i); // change() 함수의 원형 선언
 void main() {
 	int a = 10;
 	int *p = &a; // 포인터 변수 p 선언 + 변수 a의 주소를 저장
+	printf("함수 호출 이전의 a = %d\n", a); // 변수 a에 담긴 값(정수) 출력
+
+	change(p);
+	printf("함수 호출 이후의 a = %d\n", a); // 변수 a에 담긴 값(정수) 출력
+}
+
+int change(int* k) { // change() 함수가 호출되면 p의 값/a의 주소가 이 함수의 매개변수로 전달됨
+
 }
